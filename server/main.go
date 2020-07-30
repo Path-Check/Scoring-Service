1ab47d91a6d3d559f436feec4bebba071c3fb309
justdave <justdave@justdaves-MacBook-Air.local>
2020-07-29 19:23:03 -0500

    Rearranged Dockerfiles and main.go's

--- server/main.go
+++ server/main.go
@@ -2,65 +2,52 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
-	"logRPC/pb"
-	"net"
 	"os"
 	"time"
+
+	"logRPC/pb"
 
 	"google.golang.org/grpc"
 )
 
 const (
-	port = ":80"
+	address = "localhost:80"
 )
 
-type server struct {
-	pb.UnimplementedLoggerServer
-}
-
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterLoggerServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
 
-func (s *server) CreateEvent(ctx context.Context, event *pb.Event) (*pb.EventResponse, error) {
-	method := event.GetMethod()
-	timestamp := event.GetTimestamp()
-	saveToFile(ctx, event)
-	log.Printf("Event Logged: %s, at timestamp: %s", method, timestamp)
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewLoggerClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
 	eventtimestamp := time.Now().String()
-	return &pb.EventResponse{EventLogged: true, Timestamp: eventtimestamp}, nil
-}
 
-// SaveToFile saves to a json file
-func saveToFile(ctx context.Context, e *pb.Event) (bool, error) {
-	m, err := json.Marshal(e)
+	pid := int32(os.Getpid())
+
+	hostname, _ := os.Hostname()
+
+	e := &pb.Event{
+		Method:     "Test Method",
+		RequestURI: "/test",
+		RouteName:  "Test",
+		Timestamp:  eventtimestamp,
+		Pid:        pid,
+		Hostname:   hostname,
+	}
+
+	r, err := c.CreateEvent(ctx, e)
 	if err != nil {
-		log.Fatalf("failed to marshal: %v", err)
-		return false, err
+		log.Fatalf("could not log: %v", err)
 	}
-	// If the file doesn't exist, create it, or append to the file
-	l, err := os.OpenFile("logtest.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatal(err)
-		return false, err
-	}
-	if _, err := l.Write(m); err != nil {
-		log.Fatal(err)
-		return false, err
-	}
-	if err := l.Close(); err != nil {
-		log.Fatal(err)
-		return false, err
-	}
-	return true, nil
+
+	log.Printf("Event Logged: %t, at Time: %s", r.GetEventLogged(), r.GetTimestamp())
 }
