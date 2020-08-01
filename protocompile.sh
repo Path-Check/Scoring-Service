90e031b77bf179ff9fd978ee00e4aaa92fc656f2
justdave <justdave@linux.com>
2020-07-31 20:02:41 -0500

    updated protobufs

--- protocompile.sh
+++ protocompile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-protoc --proto_path=pb --go_out=plugins=grpc:pb pb/log.proto --experimental_allow_proto3_optional
+protoc --proto_path=pb --go_out=plugins=grpc:pb pb/log.proto
 protoc --proto_path=pb --go_out=plugins=grpc:pb pb/notification.proto
 
