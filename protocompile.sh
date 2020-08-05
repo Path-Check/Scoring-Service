dd22fa0d3d9c4185c4f2b87d3b3c6f0dd5f4e241
justdave <justdave@linux.com>
2020-08-04 20:31:14 -0500

    updated protocompile.sh

--- protocompile.sh
+++ protocompile.sh
@@ -4,4 +4,4 @@ protoc --proto_path=pb --go_out=plugins=grpc:pb pb/notification.proto
 
 # protoc --proto_path=pb --go_out=plugins=grpc:pb pb/log.proto
 
-grpc_tools_node_protoc --js_out=import_style=commonjs,binary:../pb/js/ --grpc_out=../pb/js --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` notification.proto
+# grpc_tools_node_protoc --js_out=import_style=commonjs,binary:../pb/js/ --grpc_out=../pb/js --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` notification.proto
