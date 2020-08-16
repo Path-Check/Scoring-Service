cad6c7df224af643d50c29e09d4f6b1c612cc21d
lmartensson <lina.martensson@gmail.com>
2020-08-15 23:10:13 -0700

    Added structs for parsing JSON as described in https://docs.google.com/document/d/1gYAwLWWIKeiBR5UfY5mCLJD0d6ftfOpl4f5kI-j50ms/edit?usp=sharing
    Initial test created to make sure it parses, more should exist.

---
+++ model/exposure.go
@@ -0,0 +1,42 @@
+package model
+
+// Apparently these type names need to start with a capital letter or things
+// won't be parsed and really, Go, you're gonna be that picky?
+type ExposureNotificationRequest struct {
+  ExposureSummaries      []ExposureSummary   `json:"exposure_summaries"`
+}
+
+type ExposureSummary struct {
+  DateReceived           int     `json:"date_received, omitempty"`
+  TimezoneOffset         int     `json:"timezone_offset, omitempty"`
+  SeqNoInDay             int     `json:"seq_no_in_day, omitempty"`
+  AttenuationDurations   AttenuationDurations     `json:"attenuation_durations, omitempty"`
+  MatchedKeyCount        int     `json:"matched_key_count, omitempty"`
+  DaysSinceLastExposure  int     `json:"days_since_last_exposure, omitempty"`
+  MaximumRiskScore       int     `json:"maximum_risk_score, omitempty"`
+  RiskScoreSum           int     `json:"risk_score_sum, omitempty"`
+}
+
+type AttenuationDurations struct {
+  Low    int `json:"low, omitempty"`
+  Medium int `json:"medium, omitempty"`
+  High   int `json:"high, omitempty"`
+}
+
+type ExposureNotificationResponse struct {
+  Notifications          []Notification       `json:"notifications"`
+}
+
+type Notification struct {
+  ExposureSummaryRefs    []ExposureSummaryRef `json:"exposure_summary_refs"`
+  DurationSeconds        int     `json:"duration_seconds"`
+  // Note: we must have dateOfExposure OR dateMostRecentExposure + matchedKeyCount, but NOT both.
+  DateOfExposure         int     `json:"date_of_exposure, omitempty"`
+  DateMostRecentExposure int     `json:"date_most_recent_exposure, omitempty"`
+  MatchedKeyCount        int     `json:"matched_key_count, omitempty"`
+}
+
+type ExposureSummaryRef struct {
+  DateReceived           int     `json:"date_received, omitempty"`
+  SeqNoInDay             int     `json:"seq_no_in_day, omitempty"`
+}
