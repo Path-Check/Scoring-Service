89f8d511468d26ea1673539bf2f72ee10d271faa
justdave <justdave@linux.com>
2020-08-17 13:32:42 -0500

    upadated testLog

--- logger/handlers/log_test.go
+++ logger/handlers/log_test.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestLog(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Client")
+	}))
+	defer server.Close()
 	type args struct {
 		w http.ResponseWriter
 		r *http.Request
@@ -15,7 +21,7 @@ func TestLog(t *testing.T) {
 		args args
 	}{
 		// TODO: Add test cases.
-		// {name: "Index", args{w: }}
+		{name: "Index", args: args{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- logger/model/exposure.go
+++ logger/model/exposure.go
@@ -21,3 +21,17 @@ type AttenuationDurations struct {
 type ExposureNotificationResponse struct {
 	ExposureSummaries []ExposureSummary `json:"exposureSummaries"`
 }
+
+// Note: we must have dateOfExposure OR dateMostRecentExposure + matchedKeyCount, but NOT both.
+type Notification struct {
+	ExposureSummaryRefs    []ExposureSummaryRef `json:"exposure_summary_refs"`
+	DurationSeconds        int                  `json:"duration_seconds"`
+	DateOfExposure         int                  `json:"date_of_exposure, omitempty"`
+	DateMostRecentExposure int                  `json:"date_most_recent_exposure, omitempty"`
+	MatchedKeyCount        int                  `json:"matched_key_count, omitempty"`
+}
+
+type ExposureSummaryRef struct {
+	DateReceived int `json:"date_received, omitempty"`
+	SeqNoInDay   int `json:"seq_no_in_day, omitempty"`
+}
